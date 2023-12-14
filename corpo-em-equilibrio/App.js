1a42458dfd539c20383662010e3328475a180f56
Marcos Medeiros <marcosbourdon@hotmail.com>
2023-12-14 16:43:46 -0300

    first commit
    Criação dos arquivos necessários para inicialização do projeto.

---
+++ corpo-em-equilibrio/App.js
@@ -0,0 +1,17 @@
+import { StyleSheet, View } from "react-native";
+import Title from "./src/components/Title";
+
+export default function App() {
+  return <View style={styles.container}>
+    <Title />
+  </View>;
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
