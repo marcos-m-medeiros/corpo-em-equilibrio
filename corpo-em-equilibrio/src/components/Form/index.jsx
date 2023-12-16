d6dead3ca088549241185355931163e6fc2f797a
Marcos Medeiros <marcosbourdon@hotmail.com>
2023-12-16 10:49:33 -0300

    Estilização do formulário
    Criação da estrutura de estilo para o formulário da aplicação.

--- corpo-em-equilibrio/src/components/Form/index.jsx
+++ corpo-em-equilibrio/src/components/Form/index.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
-import { View, Text, TextInput, Button } from "react-native";
+import { View, Text, TextInput, TouchableOpacity } from "react-native";
 import ResultImc from "./ResultIMC";
+import styles from "./style";
 
 export default function Form() {
   const [height, setHeight] = useState(null);
@@ -35,23 +36,32 @@ export default function Form() {
   }
 
   return (
-    <View>
-      <View>
-        <Text>Altura</Text>
+    <View style={styles.formContext}>
+      <View style={styles.form}>
+        <Text style={styles.formLabel}>Altura</Text>
         <TextInput
+          style={styles.input}
           onChangeText={setHeight}
           value={height}
           placeholder="Ex. 1.75"
           keyboardType="numeric"
         />
-        <Text>Peso</Text>
+        <Text style={styles.formLabel}>Peso</Text>
         <TextInput
+          style={styles.input}
           onChangeText={setWeight}
           value={weight}
           placeholder="Ex. 75.3"
           keyboardType="numeric"
         />
-        <Button onPress={() => validationImc()} title={textButton} />
+        <TouchableOpacity
+          style={styles.buttonCalculator}
+          onPress={() => {
+            validationImc();
+          }}
+        >
+          <Text style={styles.textButtonCalculator}>{textButton}</Text>
+        </TouchableOpacity>
       </View>
       <ResultImc messageResultImc={messageImc} resultImc={imc} />
     </View>
