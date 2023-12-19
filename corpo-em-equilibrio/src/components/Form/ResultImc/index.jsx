be9769dc5c1be0b1a9bff8936d168b6f8b6fad4e
Marcos Medeiros <marcosbourdon@hotmail.com>
2023-12-19 15:19:10 -0300

    Implementação do Botão "Compartilhar"
    Implementado a API Share no formulário  para compartilhamento do resultado do IMC.

--- corpo-em-equilibrio/src/components/Form/ResultImc/index.jsx
+++ corpo-em-equilibrio/src/components/Form/ResultImc/index.jsx
@@ -1,10 +1,25 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, TouchableOpacity, Share } from "react-native";
 import styles from "./style";
 
 export default function ResultImc(props) {
+  const onShare = async () => {
+    const result = await Share.share({
+      message: "Meu IMC hoje é: " + props.resultImc,
+    });
+  };
+
   return (
     <View style={styles.resultImc}>
+      <View style={styles.boxShareButton}>
+        {props.resultImc != null ? (
+          <TouchableOpacity onPress={onShare} style={styles.shared}>
+            <Text style={styles.sharedText}>Compartilhar</Text>
+          </TouchableOpacity>
+        ) : (
+          <View />
+        )}
+      </View>
       <Text style={styles.information}>{props.messageResultImc}</Text>
       <Text style={styles.numberImc}>{props.resultImc}</Text>
     </View>
